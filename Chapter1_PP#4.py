675566733bfc08f9ce59f5bdf3f1a900aafb3b96
Kevin Moreno <moren162@mail.chapman.edu>
2019-03-17 19:55:25 -0700

    Made Welcome Message and made numbers all ints.

--- Chapter1_PP#4.py
+++ Chapter1_PP#4.py
@@ -6,9 +6,9 @@ Write a program that takes years as input (as an integer) and prints out an esti
 (as an integer). Assume that the current population is 307,357,870, and assume that there are
 exactly 365 days in a year '''
 
-#Welcome print
-#has to be ints
-#Finish 
+
+print("Welcome! This program will estimate population with a birth every 7 seconds, death every 13 seconds, and a new immigrant every 35 seconds in a country.")
+print("This program will assume the current population is 307,357,870 and exactly 365 days in a year.")
 
 birth  = 7
 death = 13
@@ -18,29 +18,40 @@ immigrant = 35
 years = int(input("How many years ? : "))
 
 days = years*365
-print(days)
+
 
 hours = days*24
-print(hours)
+
 
 minutes = hours*60
-print(minutes)
+
 
 seconds = minutes*60
-print("The amount of total seconds are " + str(seconds))
+
 
 totalBirths = seconds/7
-print("The total number of births are : " + str(totalBirths))
+intTotalBirths = int(totalBirths)
+print("The total number of births are : " + str(intTotalBirths))
 
 totalDeaths = seconds/13
-print("The total number of deaths are : " + str(totalDeaths))
+intTotalDeaths = int(totalDeaths)
+print("The total number of deaths are : " + str(intTotalDeaths))
 
 totalImmigrants = seconds/35
-print("The total number of New Immigrants are : " + str(totalImmigrants))
+intTotalImmigrants = int(totalImmigrants)
+print("The total number of New Immigrants are : " + str(intTotalImmigrants))
+print("---" * 22)
 
-population = int(307357870)
-print(population)
 
-print("With the current population the totals are : ")
-newPopulation =  population + totalBirths + totalImmigrants - totalDeaths
-print("The total number of new population is : " + str(newPopulation))
+startingPopulation = int(307357870)
+
+newPopulation =  startingPopulation + totalBirths + totalImmigrants - totalDeaths
+intNewPopulation = int(newPopulation)
+
+popIncreased = intNewPopulation - startingPopulation
+
+popIncreasedPrint = f'Over {years} years, the population had increased by {popIncreased}'
+print(popIncreasedPrint)
+
+newPopulationStatement = f'The total population is : {intNewPopulation}'
+print(newPopulationStatement)
