9529ab72d74d5f90000660a97074596fff11fc25
Kevin Moreno <moren162@mail.chapman.edu>
2019-03-22 23:39:01 -0700

    Rotate letters (ROT13) and joined string together

--- Chapter5_PP#1.py
+++ Chapter5_PP#1.py
@@ -6,13 +6,10 @@ of each word, and then write the result to a file.
     '''
 
 #Need Entry message
-#Need to get rid of all punctuation
-#Need to rotate letters (ROT13)
 #Need to rotate letters (Random)
 #Have user choose ROT13 or Random
-#**Need to be able to change mid**
-#**(first, rotateMid, last).joined together
 #Print to output file
+#If length of word is less than 3, need to add to completeString
 
 
 import re
@@ -20,124 +17,153 @@ input_file = open("Chapter5_PP#1(InputFile).txt", "r")
 output_file = open("Chapter5_PP#1(OutputFile).txt", "w")
 
 
-
+completeString = ''
 for line_str in input_file:
     #Get rid of duplicate spaces in string
     stringNoDupSpaces = re.sub(' +', ' ', line_str)
     #Each word in to a list, seperated by spaces
     line_str_split = stringNoDupSpaces.split(' ')
     for word in line_str_split:
-        print("word " + word)
+        print("word --------" + word)
         #Small word does not need to be changed
-        if len(word) >= 3:
+        if len(word) >= 3: 
             #Split word into first, middle and last
             first, mid, last = word[0], word[1:-1], word[-1]
             emptyString = ''
-            new = ''
+            newMid = ''
+            newList = []   
             count = 1
-            print("Top Mid " + mid)
-            print(emptyString)
-            for i in range(0, len(mid)):
-                emptyString = emptyString + new
-                print(emptyString)
-                #if i == len(mid)-1:
-                    #mid = emptyString
-                    #print("This is mid empty!!" + mid)
-                    #break
-                if count == len(mid):
-                    mid = emptyString
-                    print(mid + " from if statement")
+            #Loop through length of the middle word
+            for i in range(0, len(mid) + 1):
+                #Saving string after changing all letters
+                if count == len(mid) + 1:
+                    #Combine the new middle characters
+                    finalMid = "".join(newMid)
+                    #Combine the new middle string with rest of word
+                    emptyString = first + finalMid + last
+                    #Add new word to whole string
+                    completeString += emptyString + " "
                 elif mid[i] == 'a':
                     new = chr(ord(mid[i]) + 13)
+                    newMid += new
                     count+=1
                 elif mid[i] == 'b':
                     new = chr(ord(mid[i]) + 13)
+                    newMid += new
+                
                     count+=1
                 elif mid[i] == 'c':
                     new = chr(ord(mid[i]) + 13)
+                    newMid += new
+                    
                     count+=1
                 elif mid[i] == 'd':
                     new = chr(ord(mid[i]) + 13)
+                    newMid += new
+            
                     count+=1
                 elif mid[i] == 'e':
                     new = chr(ord(mid[i]) + 13)
+                    newMid += new
+                    
                     count+=1
                 elif mid[i] == 'f':
                     new = chr(ord(mid[i]) + 13)
+                    newMid += new
+                
                     count+=1
                 elif mid[i] == 'g':
                     new = chr(ord(mid[i]) + 13)
+                    newMid += new
+                
                     count+=1
                 elif mid[i] == 'h':
                     new = chr(ord(mid[i]) + 13)
+                    newMid += new
+                
                     count+=1
                 elif mid[i] == 'i':
                     new = chr(ord(mid[i]) + 13)
+                    newMid += new
+        
                     count+=1
                 elif mid[i] == 'j':
                     new = chr(ord(mid[i]) + 13)
+                    newMid += new
                     count+=1
                 elif mid[i] == 'k':
                     new = chr(ord(mid[i]) + 13)
+                    newMid += new
                     count+=1
                 elif mid[i] == 'l':
                     new = chr(ord(mid[i]) + 13)
+                    newMid += new
                     count+=1
                 elif mid[i] == 'm':
                     new = chr(ord(mid[i]) + 13)
+                    newMid += new
                     count+=1
                 elif mid[i] == 'n':
                     new = chr(ord(mid[i]) - 13)
+                    newMid += new
                     count+=1
                 elif mid[i] == 'o':
                     new = chr(ord(mid[i]) - 13)
+                    newMid += new
                     count+=1
                 elif mid[i] == 'p':
                     new = chr(ord(mid[i]) - 13)
+                    newMid += new
                     count+=1
                 elif mid[i] == 'q':
                     new = chr(ord(mid[i]) - 13)
+                    newMid += new
                     count+=1
                 elif mid[i] == 'r':
                     new = chr(ord(mid[i]) - 13)
+                    newMid += new
                     count+=1
                 elif mid[i] == 's':
                     new = chr(ord(mid[i]) - 13)
+                    newMid += new
                     count+=1
                 elif mid[i] == 't':
                     new = chr(ord(mid[i]) - 13)
+                    newMid += new
                     count+=1
                 elif mid[i] == 'u':
                     new = chr(ord(mid[i]) - 13)
+                    newMid += new
                     count+=1
                 elif mid[i] == 'v':
                     new = chr(ord(mid[i]) - 13)
+                    newMid += new
                     count+=1
                 elif mid[i] == 'w':
                     new = chr(ord(mid[i]) - 13)
+                    newMid += new
                     count+=1
                 elif mid[i] == 'x':
                     new = chr(ord(mid[i]) - 13)
+                    newMid += new
                     count+=1
                 elif mid[i] == 'y':
                     new = chr(ord(mid[i]) - 13)
+                    newMid += new
                     count+=1
                 elif mid[i] == 'z':
                     new = chr(ord(mid[i]) - 13)
+                    newMid += new
                     count+=1
-                '''count = 0
-                if 
-                if ord(letter) > 96 and ord(letter) < 110:
-                    newLetter = chr(ord(letter) + 13)
-                    letter = newLetter
-                    
                 else:
-                    newLetter = chr(ord(letter) - 13)
-                    letter = newLetter
-                    print("2 " + letter)
-            print(mid)
-            
-    
+                    newMid += mid[i]
+                    count+=1
+
+
+print(stringNoDupSpaces)
+print()
+print()
+print(completeString)
 
   
             
