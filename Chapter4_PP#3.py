40ef630389afb62a38f12a689f490caa116c2914
Kevin Moreno <moren162@mail.chapman.edu>
2019-04-01 13:37:56 -0700

    Entry message and while loop.

--- Chapter4_PP#3.py
+++ Chapter4_PP#3.py
@@ -10,26 +10,31 @@ Write a program that repeatedly prompts for an english word to translate
 into Pig Latin and prints the translated word. If the user enters a period,
 halt the program.'''
 
-#Entry Message
-#User exit "."
-userWord = input("Which word would you like to translate in to Pig Latin?: ")
-vowels = 'aeiou'
-vowelFirst = "yay"
-vowelConst = "ay"
-empty = ""
-if userWord[0] in vowels:
-    newWord = userWord + vowelFirst
-    print(newWord)
-else:
-    for i in userWord:
-        if i in vowels:
-            userWord = userWord + vowelConst
-            print("User word is " + userWord)
-            break
-        empty = userWord[0]
-        print("Empty " + empty)
-        userWord = userWord[1:]
-        print("User Word after move " + userWord)
-        userWord = userWord + empty
-        print("Together User word " + userWord)
-    
+
+print("Welcome, this program will change any word to Pig Latin.")
+print("If you want to exit the application please enter '.' only")
+while True:
+    userWord = input("Which word would you like to translate in to Pig Latin? '.' to Exit : ")
+    if userWord == ".":
+        break
+    vowels = 'aeiou'
+    vowelFirst = "yay"
+    vowelConst = "ay"
+    empty = ""
+    print("Your orignial word is : " + userWord)
+    if userWord[0] in vowels:
+        newWord = userWord + vowelFirst
+        print(newWord)
+    else:
+        for i in userWord:
+            if i in vowels:
+                userWord = userWord + vowelConst
+                print('The word in pig lating is : '  + userWord)
+                break
+            empty = userWord[0]
+            userWord = userWord[1:]
+            userWord = userWord + empty
+            
+
+print("Have a great day!")
+        
