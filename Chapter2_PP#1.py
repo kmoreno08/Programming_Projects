3c17ed433a8c31dc8cbfac7a0e6d6ff70e10c77b
Kevin Moreno <moren162@mail.chapman.edu>
2019-04-02 18:13:53 -0700

    Entry message.

--- Chapter2_PP#1.py
+++ Chapter2_PP#1.py
@@ -8,12 +8,17 @@ two grains on the second square, four on the third, eight on the fourth. Board
 has 64 sqaures. Write a program to determine the following:
  (a) How many total grains of wheat did the ruler have to pay the inventor?
  (b) A wheat grain weighs approximately 50 mg. How much did the wheat weigh?
- (c) Pick a region and dtermine how deeply that region would be covered
-     with that quantity of wheat.
 '''
-#Fix question
-#Fix Miligrams
-#Excercise C
+
+
+
+print("A local ruler was pleased with the invention of chess that he offered gold.")
+print("The inventor suggested an alternative reward: he would get on grain of wheat")
+print("on the first square, two grains on the second square, four on the third and so on.")
+print()
+print("This program will determine how many total grain of wheat did the ruler have to pay the inventor")
+print("and how much did that wheat weight in miligrams.")
+print()
 
 #Excercise A
 sum = 1
@@ -23,19 +28,17 @@ mult = 2
 for i in range(1,65):
     sum = sum + (mult)
     mult = mult * 2
-    print("Count " + str(count))
-    print("Total " + (str(sum)))
-    print("----" * 22)
     count = count + 1
 
 message = f'The total grains of wheat is 36,893,488,147,419,103,231'
 print(message)
 
 #Excercise B
-totalGrain = 36893488147419103231
+totalGrain = 36_893_488_147_419_103_231
 weightPerGrainMG = 50
 totalGrainWeightMG = weightPerGrainMG * totalGrain
 print("Total weight in miligrams is " + str(totalGrainWeightMG))
+
 
     
 
