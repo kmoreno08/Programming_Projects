678e624a32b6aa702d2aea4df7325483f4a2643f
Kevin Moreno <moren162@mail.chapman.edu>
2019-04-06 05:50:01 -0700

    Functions and while loop for user input.

--- Chapter4_PP#2.py
+++ Chapter4_PP#2.py
@@ -4,72 +4,53 @@ into a predifined story. The predefined story has placeholders for those
 words that get replaced by the values prompted for.'''
 
 
-def exit_program(input):
-    lower_input = input.lower()
-    if lower_input == "exit":
-        exit = False
-        return False
+def entry_message():
+    print("Welcome to the Mad Libs generator!")
+    print("Please enter your word when prompted.")
+    print("To exit the program please type 'exit' at anytime.")
+    print("This is your original message. ")
+    print("---" * 20)
     
-        #print("Function")
-        #print(exit)
-        #return exit
+def mad_lib_message():
+    print('''Once upon a time in the middle of a ADJECTIVE_ONE NOUN_ONE stood a
+    ADJECTIVE_TWO NOUN_TWO, the home of a ADJECTIVE_ONE ADJECTIVE_THREE
+    NOUN_THREE known to everyone as GIRLS_NAME.''')
 
-def original_message():
-    print('''Once upon a time in the middle of a ADJECTIVE_ONE NOUN_ONE stood a\
-    ADJECTIVE_TWO NOUN_TWO, the home of a ADJECTIVE_ONE ADJECTIVE_THREE\
-    NOUN_THREE known to everyone as GIRLS_NAME''')
-    
-    
+def mad_lib_replaced(adj_one, noun_one, adj_two, noun_two,
+                     adj_three, noun_three, girls_name):
+    mad_lib_replaced = f'''Once upon a time in the middle of {adj_one} {noun_one} stood a
+{adj_two} {noun_two}, the home of a {adj_one} {adj_three}
+{noun_three} known to everyone as {girls_name}.'''
+    return mad_lib_replaced
 
-print("Welcome to the Mad Libs generator!")
-print("This is your original message. ")
-print("---" * 20)
-original_message()
-print("---" * 20)
-print("Please enter your word when prompted.")
-print("To exit the program please type 'exit' at anytime.")
 
-#Keep asking user - While loop
-#Put in Function
-#How to exit with function
+entry_message()
+mad_lib_message()
 
-exit_program = True
-while exit_program:
-    print(exit_program)
-    #All users inputs needed
-    adjOne = input("Please enter an adjective. : ")
-    exitProgram(adjOne)
-    print(exit)
-    nounOne = input("Plese enter a noun. : ")
-    adjTwo = input("Please enter another adjective. : ")
-    nounTwo = input("Please enter another noun. : ")
-    adjThree = input("Please enter the last adjective. : ")
-    nounThree = input("Please enter the last noun. : ")
-    girlName = input("Please enter a girls name. : ")
-    #Original message
-    original = '''Once upon a time in the middle of a ADJECTIVE_ONE NOUN_ONE stood a\
-    ADJECTIVE_TWO NOUN_TWO, the home of a ADJECTIVE_ONE ADJECTIVE_THREE\
-    NOUN_THREE known to everyone as GIRLS_NAME'''
-    #Adjective #1
-    replaceAdjOne = '''Once upon a time in the middle of a ADJECTIVE_ONE
-    '''.replace('ADJECTIVE_ONE', adjOne)
-    #Noun # 1
-    replaceNounOne = "NOUN_ONE stood a ".replace('NOUN_ONE', nounOne)
-    #Adjective #2
-    replaceAdjTwo = "ADJECTIVE_TWO ".replace('ADJECTIVE_TWO',adjTwo)
-    #Noun #2
-    replaceNounTwo = "NOUN_TWO, the home of a ".replace('NOUN_TWO', nounTwo)
-    #Adjective #1
-    replaceAdjOneSelf = "ADJECTIVE_ONE ".replace("ADJECTIVE_ONE ",adjOne)
-    #Adjective #3
-    replaceAdjThree = " ADJECTIVE_THREE ".replace('ADJECTIVE_THREE', adjThree)
-    #Noun #3
-    replaceNounThree = "NOUN_THREE known to everyone as ".replace("NOUN_THREE",nounThree)
-    #Girls name
-    replaceGirlName = "GIRLS_NAME".replace("GIRLS_NAME",girlName)
+#Empty strings
+adj_one = ""
+noun_one = ""
+adj_two = ""
+noun_two = ""
+adj_three = ""
+noun_three = ""
+girls_name = ""
 
-    #Replaced original message with user inputs
-    replaceOriginal = f'{replaceAdjOne}{replaceNounOne}{replaceAdjTwo}{replaceNounTwo}{replaceAdjOneSelf}{replaceAdjThree}{replaceNounThree}{replaceGirlName}'
-    print(replaceOriginal)
+while True:
+    #If not all 7 values are reached, will exit program.
+    try:
+        print("To exit: input less than 7 values. ")
+        #All users inputs in list comprehension
+        adj_one, noun_one, adj_two, noun_two, adj_three, noun_three, girls_name = [x for x in input("Please enter 7 values seperated by space: ").split()]
+    except:
+        print("Not all 7 values were put in correctly, exiting program.")
+        break
 
+    mad_lib_replaced = mad_lib_replaced(adj_one, noun_one, adj_two, noun_two,
+                     adj_three, noun_three, girls_name)
+    print("__" * 20)
+    print(mad_lib_replaced)
+    print("**" * 20)
+                                        
+print("Have a great day!")
 
