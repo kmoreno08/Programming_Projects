dd74b4bfcb2b33be824131ada5f1f41579607089
Kevin Moreno <moren162@mail.chapman.edu>
2019-04-11 13:09:56 -0700

    Try/Except for Non-Alphanumeric characters.

--- Chapter5_PP#1.py
+++ Chapter5_PP#1.py
@@ -10,7 +10,7 @@ of each word, and then write the result to a file.
 #Have user choose ROT13 or Random
 #Print to output file
 #If length of word is less than 3, need to add to completeString
-##Use string.index(i) + shift) % 26 (Function) then join code
+
 
 
 import re
@@ -36,9 +36,14 @@ for line_str in input_file:
             fullWord = ''
             newMid = ''
             for i in mid:
-                newIndexLetter = ((alphabet.index(i) + shift) % 26)
-                newMid += alphabet[newIndexLetter]
-                count += 1
+                #Try/Except for characters that are not letters
+                try:
+                    newIndexLetter = ((alphabet.index(i) + shift) % 26)
+                    newMid += alphabet[newIndexLetter]
+                    count += 1
+                except ValueError:
+                    newMid += i
+                    count += 1
                 if count == len(mid):
                     newIndexLetter = ((alphabet.index(i) + shift) % 26)
                     newMid += alphabet[newIndexLetter]
@@ -48,134 +53,7 @@ for line_str in input_file:
                     completeString += fullWord + " "
                     print("THE END!")
             print(completeString)
-            '''
-            newList = []   
-            count = 1
-            #Loop through length of the middle word
-            for i in range(0, len(mid) + 1):
-                #Saving string after changing all letters
-                if count == len(mid) + 1:
-                    #Combine the new middle characters
-                    finalMid = "".join(newMid)
-                    #Combine the new middle string with rest of word
-                    emptyString = first + finalMid + last
-                    #Add new word to whole string
-                    completeString += emptyString + " "
-                elif mid[i] == 'a':
-                    new = chr(ord(mid[i]) + 13)
-                    newMid += new
-                    count+=1
-                elif mid[i] == 'b':
-                    new = chr(ord(mid[i]) + 13)
-                    newMid += new
-                
-                    count+=1
-                elif mid[i] == 'c':
-                    new = chr(ord(mid[i]) + 13)
-                    newMid += new
-                    
-                    count+=1
-                elif mid[i] == 'd':
-                    new = chr(ord(mid[i]) + 13)
-                    newMid += new
-            
-                    count+=1
-                elif mid[i] == 'e':
-                    new = chr(ord(mid[i]) + 13)
-                    newMid += new
-                    
-                    count+=1
-                elif mid[i] == 'f':
-                    new = chr(ord(mid[i]) + 13)
-                    newMid += new
-                
-                    count+=1
-                elif mid[i] == 'g':
-                    new = chr(ord(mid[i]) + 13)
-                    newMid += new
-                
-                    count+=1
-                elif mid[i] == 'h':
-                    new = chr(ord(mid[i]) + 13)
-                    newMid += new
-                
-                    count+=1
-                elif mid[i] == 'i':
-                    new = chr(ord(mid[i]) + 13)
-                    newMid += new
-        
-                    count+=1
-                elif mid[i] == 'j':
-                    new = chr(ord(mid[i]) + 13)
-                    newMid += new
-                    count+=1
-                elif mid[i] == 'k':
-                    new = chr(ord(mid[i]) + 13)
-                    newMid += new
-                    count+=1
-                elif mid[i] == 'l':
-                    new = chr(ord(mid[i]) + 13)
-                    newMid += new
-                    count+=1
-                elif mid[i] == 'm':
-                    new = chr(ord(mid[i]) + 13)
-                    newMid += new
-                    count+=1
-                elif mid[i] == 'n':
-                    new = chr(ord(mid[i]) - 13)
-                    newMid += new
-                    count+=1
-                elif mid[i] == 'o':
-                    new = chr(ord(mid[i]) - 13)
-                    newMid += new
-                    count+=1
-                elif mid[i] == 'p':
-                    new = chr(ord(mid[i]) - 13)
-                    newMid += new
-                    count+=1
-                elif mid[i] == 'q':
-                    new = chr(ord(mid[i]) - 13)
-                    newMid += new
-                    count+=1
-                elif mid[i] == 'r':
-                    new = chr(ord(mid[i]) - 13)
-                    newMid += new
-                    count+=1
-                elif mid[i] == 's':
-                    new = chr(ord(mid[i]) - 13)
-                    newMid += new
-                    count+=1
-                elif mid[i] == 't':
-                    new = chr(ord(mid[i]) - 13)
-                    newMid += new
-                    count+=1
-                elif mid[i] == 'u':
-                    new = chr(ord(mid[i]) - 13)
-                    newMid += new
-                    count+=1
-                elif mid[i] == 'v':
-                    new = chr(ord(mid[i]) - 13)
-                    newMid += new
-                    count+=1
-                elif mid[i] == 'w':
-                    new = chr(ord(mid[i]) - 13)
-                    newMid += new
-                    count+=1
-                elif mid[i] == 'x':
-                    new = chr(ord(mid[i]) - 13)
-                    newMid += new
-                    count+=1
-                elif mid[i] == 'y':
-                    new = chr(ord(mid[i]) - 13)
-                    newMid += new
-                    count+=1
-                elif mid[i] == 'z':
-                    new = chr(ord(mid[i]) - 13)
-                    newMid += new
-                    count+=1
-                else:
-                    newMid += mid[i]
-                    count+=1'''
+
 
 print()
 print("---" * 20)
