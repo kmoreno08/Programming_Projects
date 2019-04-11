05bea3a1068e8829d0f2faa898456f99c0423425
Kevin Moreno <moren162@mail.chapman.edu>
2019-04-11 16:55:34 -0700

    Added comments

--- Chapter5_PP#1.py
+++ Chapter5_PP#1.py
@@ -27,22 +27,23 @@ if userChoice == 'y':
     shift = userInputShift
     print("You have chosen a shift of " + str(shift))
 else:
+    #Random from 1 - 26. 26 would result in no movement
     random = random.randint(1, 26)
     print()
     print("The computer has chosen ROT" + str(random))
     shift = random
 
-
+#Alphabet string used to find the correct index, once found will shift from there.
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 finalString = ''
-newIndexLetter = 0
+newIndexNumber = 0
 for line_str in inputFile:
     #Get rid of duplicate spaces in string
     stringNoDupSpaces = re.sub(' +', ' ', line_str)
     #Each word in to a list, seperated by spaces
     line_str_split = stringNoDupSpaces.split(' ')
     for word in line_str_split:
-        #if len(word) >= 3:
+        #If length of word is more then or equal to 3
         if len(word) >= 3:
             count = 1
             #Split word into first, middle and last
@@ -52,9 +53,13 @@ for line_str in inputFile:
             for i in mid:
                 #At end of each word save to finalString to print
                 if count == len(mid):
-                    newIndexLetter = ((alphabet.index(i) + shift) % 26)
-                    newMid += alphabet[newIndexLetter]
+                    #Find letter in alphabet and save index number then shift
+                    newIndexNumber = ((alphabet.index(i) + shift) % 26)
+                    #Join the middle of the word after using new index.
+                    newMid += alphabet[newIndexNumber]
+                    #Join the whole word together
                     fullWord = first + newMid + last
+                    #Add complete word to final string.
                     finalString += fullWord + " "
                 #Try/Except for characters that are not letters
                 try:
@@ -70,13 +75,18 @@ for line_str in inputFile:
 
 
 print()
+#Print before shift
 print("BEFORE: ")
 print(stringNoDupSpaces)
 print()
 print()
 print("AFTER: ")
+#Print after shift
 print(finalString)
+#Write to output file
 outputFile.write(finalString)
+
+#Close both files
 inputFile.close()
 outputFile.close()
 
