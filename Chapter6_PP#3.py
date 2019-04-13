52c6872b18d333dfe66ee4af8eaa8fb4dfd52b76
Kevin Moreno <moren162@mail.chapman.edu>
2019-04-12 20:30:54 -0700

    User choice on total bean count.

--- Chapter6_PP#3.py
+++ Chapter6_PP#3.py
@@ -6,36 +6,52 @@ to take some number of beans from the pile during each turn. Player who takes
 the last bean loses.'''
 
 #Entry message
-#Take input from both players to choose how many beans need to be taken
-#Total bean count is random, 16 max 4 min (user choses)
-#needs to be in functions
 
 
 def user_a_amount(total_beans):
     user_a_int = int(input("How many beans would you like to take user A? "))
     total_beans -= user_a_int
-    print(f'{total_beans} number of bean(s) are left!')
+    print(f'{total_beans} bean(s) are left!')
     return total_beans
 
 def user_b_amount(total_beans):
     user_b_int = int(input("How many beans user B? "))
     total_beans -= user_b_int
-    print(f'{total_beans} number of bean(s) are left!')
+    print(f'{total_beans} bean(s) are left!')
     return total_beans
     
                   
 
 import random
+#Random number from 4 to 16
+random = random.randint(4, 16)
 
-total_beans = 16
+print("Would you like to have a random choice of beans (min: 4, max: 16) or")
+print("have 16 beans in total?")
+player_choice = input("Random? (Please type y/n) : ")
+player_choice.lower()
+#If yes, then total beans are a random number
+if player_choice == 'y':
+    total_beans = random
+    print(f'Random has been chosen! You have {total_beans} beans in total.')
+#Total beans are 16
+else:
+    total_beans = 16
+    print("You have 16 total beans!")
+
 print("whoever takes the last bean loses!")
+#While loop to keep asking user until all beans are gone
 while True:
+    #User A takes a turn
     total_beans = user_a_amount(total_beans)
+    #User A loses if takes last bean
     if total_beans <= 0:
         print("Sorry user A, you have taken the last bean!")
         print("The winner is User B.")
         break
+    #User B takes a turn
     total_beans = user_b_amount(total_beans)
+    #User B loses if takes last bean
     if total_beans <= 0:
         print("Sorry user B, you have lost!")
         print("The winner is User A.")
