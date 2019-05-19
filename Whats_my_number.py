1ebbc65d60e30d3b49c5a4f4ca3b762e770c1351
Kevin Moreno <moren162@mail.chapman.edu>
2019-05-18 23:24:23 -0700

    Initial Commit

---
+++ Whats_my_number.py
@@ -0,0 +1,13 @@
+'''Between 1 and 1000, there is only 1 number that meets the following criteria.
+While it could be manually figured out with pen and paper, it would be much
+more efficient to write a program that would do this for you.
+With that being said, your goal is to find out which number meets these criteria.
+
+1) The number has two more digits
+2) The number is prime
+3) The number does NOT contain a 1 or 7 in it
+4) The sum of all of the digits is less than or equal to 10
+5) First two digits add up to be odd
+6) The second to last digit is even
+7) The last digit is equal to how many digits are in the number
+'''
