769d2e7b55c9199e0fdc9c1d5f9f92e13aa54ccf
Kevin Moreno <moren162@mail.chapman.edu>
2019-05-19 09:10:15 -0700

    Initial Commit

---
+++ hangman.py
@@ -0,0 +1,14 @@
+#Hangman Game
+''' Game to guess the word.
+ 1) The user needs to be able to input letter guesses.
+ 2) A limit should also be set on how many guesses they can use.
+ 3) Keep notifying the user of the remaining turns.
+Text file will be used for words. There will also be a check if the user has actually inputed
+a single letter, to check if the inputted is in the hidden word (and if it is, how many times
+it appears), to print letters, and a count variable to limit guesses.
+'''
+
+
+
+
+print("Welcome to Hangman")
