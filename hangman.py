495c1536b71929971e6a973bf8f43c587aea8a8b
Kevin Moreno <moren162@mail.chapman.edu>
2019-05-20 14:54:32 -0700

    Basic functionality Test

--- hangman.py
+++ hangman.py
@@ -8,7 +8,59 @@ a single letter, to check if the inputted is in the hidden word (and if it is, h
 it appears), to print letters, and a count variable to limit guesses.
 '''
 
-
+#Text file from "dwyl" on Github
 
 
 print("Welcome to Hangman")
+print("Please guess a letter")
+guesses_left = int(input("How many guesses would you like? "))
+
+
+
+
+word = "Dog"
+word_lower = word.lower()
+print(word_lower)
+
+
+#Read text document to grab word - Random
+
+
+
+#Make word in to a list
+list_word = list(word_lower)
+
+#Program will exit if no guesses left
+while guesses_left != 0:
+    #If guess all letters in word
+    print("Congrats! You have guessed the word!")
+    print("The word is " + word)
+    while list_word != []:
+        #Break out of loop if no guesses left
+        if guesses_left == 0:
+            print("You have no remaing guesses left!")
+            print("Letter(s) that have still not been guessed: " + str(list_word))
+            print("The whole word is : " + word)
+            break
+        #Keep asking for letter
+        print("Please enter a letter: ")
+        user_letter = input("Enter letter : ")
+        #Check if letter is in word
+        if user_letter in list_word:
+            print("Yes it is in here")
+            guesses_left -= 1
+            #Iterate through each letter in word
+            for letter in list_word:
+                #Letter is in word then remove from list
+                if letter == user_letter:
+                    list_word.remove(letter)
+                    print(list_word)
+            print("Your remaining number of guesses are " + str(guesses_left))
+        #Letter is not in word   
+        else:
+            print("nope, sorry!")
+            guesses_left -= 1
+            print("Your remaining number of guesses are " + str(guesses_left))
+
+#Exit program    
+print("You have exited the program.")
