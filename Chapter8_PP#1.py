6159c70038e68659ccb939384e7046ea3f458e7b
Kevin Moreno <moren162@mail.chapman.edu>
2019-06-11 17:49:40 -0700

    Monthly avg-price Calculated

--- Chapter8_PP#1.py
+++ Chapter8_PP#1.py
@@ -5,7 +5,7 @@ and tell us the best six months and the worse six months'''
 #Entry message
 #getMonthlyAverages function
 #printInfo function
-
+#DRY code
 
 def getDataList(fileName):
     with open(fileName, 'r') as dataFile:
@@ -19,7 +19,8 @@ def getDataList(fileName):
                 #dataList.append(lineStr.strip().split(','))
         return dataList
 
-dataList = getDataList('AMZN(1).csv')
+dataList = getDataList('AMZN.csv')
+
 
 
 count = 1
@@ -29,6 +30,7 @@ date = ""
 ave_price_num = 0
 ave_price_den = 0
 ave_price = 0
+month_list = []
 
 for i in dataList:
     date = dataList[count][0]
@@ -36,20 +38,149 @@ for i in dataList:
     close_float = float(dataList[count][5])
     ave_price_num += volume_int * close_float
     ave_price_den += volume_int
-    print("Volume " + str(volume_int))
-    print("close price " + str(close_float))
-    print("Ave price Numerator " + str(ave_price_num))
-    print("Ave price den " + str(ave_price_den))
-    print("-----------------" * 2)
+    #print("Volume " + str(volume_int))
+    #print("close price " + str(close_float))
+    #print("Ave price Numerator " + str(ave_price_num))
+    #print("Ave price den " + str(ave_price_den))
+    #print("-----------------" * 2)
     count += 1
-    if count > 22:
+    
+    #June/18
+    if count == 22:
         ave_price = ave_price_num/ave_price_den
-        print("average price " + str(ave_price))
-        print("Average price for June 2018 is : " + str(ave_price))
+        tuple_june = ("06/2018", ave_price)
+        print(tuple_june)
+        month_list.append(tuple_june)
+        print(month_list)
+        ave_price_num = 0
+        ave_price_den = 0
+        ave_price = 0
+        close_float = 0
+        volume_int = 0
+    #July/18
+    elif count == 43:
+        ave_price = ave_price_num/ave_price_den 
+        tuple_july = ("07/2018", ave_price)
+        print(tuple_july)
+        month_list.append(tuple_july)
+        print(month_list)
+        ave_price_num = 0
+        ave_price_den = 0
+        ave_price = 0
+        close_float = 0
+        volume_int = 0 
+    #August/18
+    elif count == 66:
+        ave_price = ave_price_num/ave_price_den 
+        tuple_aug = ("08/2018", ave_price)
+        print(tuple_aug)
+        month_list.append(tuple_aug)
+        ave_price_num = 0
+        ave_price_den = 0
+        ave_price = 0
+        close_float = 0
+        volume_int = 0 
+    #September/18
+    elif count == 85:
+        ave_price = ave_price_num/ave_price_den 
+        tuple_sep = ("09/2018", ave_price)
+        print(tuple_sep)
+        month_list.append(tuple_sep)
+        ave_price_num = 0
+        ave_price_den = 0
+        ave_price = 0
+        close_float = 0
+        volume_int = 0
+    #October/18
+    elif count == 108:
+        ave_price = ave_price_num/ave_price_den 
+        tuple_oct = ("10/2018", ave_price)
+        print(tuple_oct)
+        month_list.append(tuple_oct)
+        ave_price_num = 0
+        ave_price_den = 0
+        ave_price = 0
+        close_float = 0
+        volume_int = 0
+    #November/18
+    elif count == 129:
+        ave_price = ave_price_num/ave_price_den 
+        tuple_nov = ("11/2018", ave_price)
+        print(tuple_nov)
+        month_list.append(tuple_nov)
+        ave_price_num = 0
+        ave_price_den = 0
+        ave_price = 0
+        close_float = 0
+        volume_int = 0
+    #December/18
+    elif count == 148:
+        ave_price = ave_price_num/ave_price_den 
+        tuple_dec = ("12/2018", ave_price)
+        print(tuple_dec)
+        month_list.append(tuple_dec)
+        ave_price_num = 0
+        ave_price_den = 0
+        ave_price = 0
+        close_float = 0
+        volume_int = 0
+    #Jan/19
+    elif count == 169:
+        ave_price = ave_price_num/ave_price_den 
+        tuple_jan = ("01/2019", ave_price)
+        print(tuple_jan)
+        month_list.append(tuple_jan)
+        ave_price_num = 0
+        ave_price_den = 0
+        ave_price = 0
+        close_float = 0
+        volume_int = 0
+    #Feb/19
+    elif count == 188:
+        ave_price = ave_price_num/ave_price_den 
+        tuple_feb = ("02/2019", ave_price)
+        print(tuple_feb)
+        month_list.append(tuple_feb)
+        ave_price_num = 0
+        ave_price_den = 0
+        ave_price = 0
+        close_float = 0
+        volume_int = 0
+    #March/19
+    elif count == 209:
+        ave_price = ave_price_num/ave_price_den 
+        tuple_march = ("03/2019", ave_price)
+        print(tuple_march)
+        month_list.append(tuple_march)
+        ave_price_num = 0
+        ave_price_den = 0
+        ave_price = 0
+        close_float = 0
+        volume_int = 0
+    #April/19
+    elif count == 230:
+        ave_price = ave_price_num/ave_price_den 
+        tuple_april = ("04/2019", ave_price)
+        print(tuple_april)
+        month_list.append(tuple_april)
+        ave_price_num = 0
+        ave_price_den = 0
+        ave_price = 0
+        close_float = 0
+        volume_int = 0
+    #May/19
+    elif count == 252:
+        ave_price = ave_price_num/ave_price_den 
+        tuple_may = ("05/2019", ave_price)
+        print(tuple_may)
+        month_list.append(tuple_may)
+        ave_price_num = 0
+        ave_price_den = 0
+        ave_price = 0
+        close_float = 0
+        volume_int = 0
         break
- 
-          
-'''for i in dataList:
-    for j in i:
-        dataList[i][j] for i, j in dataList if i...'''
+
+print("Have exited loop")
+print(month_list)
             
