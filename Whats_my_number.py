888e199bc38f18cf09ae1c09fdd8b2665f3b2fd8
Kevin Moreno <moren162@mail.chapman.edu>
2019-07-28 15:48:44 -0700

    Number is prime completed.

--- Whats_my_number.py
+++ Whats_my_number.py
@@ -3,7 +3,7 @@ While it could be manually figured out with pen and paper, it would be much
 more efficient to write a program that would do this for you.
 With that being said, your goal is to find out which number meets these criteria.
 
-1) The number has two more digits
+1) The number has at least two digits
 2) The number is prime
 3) The number does NOT contain a 1 or 7 in it
 4) The sum of all of the digits is less than or equal to 10
@@ -12,11 +12,22 @@ With that being said, your goal is to find out which number meets these criteria
 7) The last digit is equal to how many digits are in the number
 '''
 
-i = 1
+#Number has at least two digits
+lower = 900
+upper = 1000
 
-#Number has 2 digits
-for i in range(1, 1000):
-    if i >= 10:
-        if 
-    else:
-        print("Exiting loop")
+
+#Prime numbers only
+for num in range(lower, upper + 1):
+    if num > 1:
+        for i in range (2, num):
+            if (num % i) == 0:
+                break
+        else:
+            print(num)
+            #does not contain a 1 or 7 in it
+            
+    
+  
+    
+        
