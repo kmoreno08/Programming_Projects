ff1a0497487708e6322fe7648b9e83bdf3530056
Kevin Moreno <moren162@mail.chapman.edu>
2019-07-31 20:06:13 -0700

    Sum of all digits is less than or equal to 10

--- Whats_my_number.py
+++ Whats_my_number.py
@@ -6,14 +6,14 @@ With that being said, your goal is to find out which number meets these criteria
 1) The number has at least two digits --complete
 2) The number is prime --complete
 3) The number does NOT contain a 1 or 7 in it --complete
-4) The sum of all of the digits is less than or equal to 10
+4) The sum of all of the digits is less than or equal to 10 --complete
 5) First two digits add up to be odd
 6) The second to last digit is even
 7) The last digit is equal to how many digits are in the number
 '''
 
 #Number has at least two digits
-lower = 100
+lower = 10
 upper = 1000
 
 #Empty list for all numbers
@@ -46,10 +46,27 @@ for num in range(lower, upper + 1):
                     #Remove number from list
                     final_num_list.remove(num)
                     break
-                #Sum of all digits is less than or equal to 10
+                #Keep number in final_num_list
                 else:
-                    print(final_num_list)
-                    
+                    pass
+
+
+#Sum of all digits is less than or equal to 10
+#Reverse list for index out of range error
+for index in range(len(final_num_list)-1, -1, -1):
+    #Split number in to list
+    num_list = [int(d) for d in str(final_num_list[index])]
+    #Get sum of total number
+    sum_num = sum(num_list)
+    #If sum is more then 10, remove from list
+    if sum_num > 10:
+        #Remove from list
+        del final_num_list[index]
+    else:
+        pass
+
+print(final_num_list)
+
     
   
     
