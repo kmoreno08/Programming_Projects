b75a0d97970df7b4eab8322e31cd197bb312236d
Kevin S. Moreno <moren162@mail.chapman.edu>
2019-08-02 15:34:25 -0700

    Delete Chapter11_PP#1.py

--- Chapter11_PP#1.py
+++
@@ -1,5 +0,0 @@
-#Solitaire
-'''Writing a program to play the popular card game of solitaire. Suites are represented
-by their first letter: 'S', 'D', 'H', 'C'. Each suit has 13 cards from ace,2-10,jack, queen,
-king with ranks 1-13. '''
-
